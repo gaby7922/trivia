88b98cba219ee693587fafafc5cd111e271cc83c
gaby7922 <gaby7922@gmail.com>
2018-05-08 10:25:27 -0300

    mejorar apariencia

---
+++ app.js
@@ -0,0 +1,56 @@
+alert("¡Bienvenida!");
+var nombre = prompt("¿Como te llamas?");
+document.getElementById("nombre_persona").innerHTML=nombre;
+alert("Gusto en escribirte: " + nombre);
+var play = prompt("¿Quieres jugar?");
+
+//* colocar la variable en minuscula *//
+play = play.toLowerCase();
+if (play == "si") {
+	var age = prompt("¿A partir de que edad puedes particpar en el Bootcamp de Laboratoria?");
+//* primera pregunta *//
+	if (age === "18") {
+		alert("!Correcto¡");
+		pregunta_1 = true;
+		document.getElementById("respuesta_1").innerHTML='Correcto';
+	} else {
+		alert("¡Respuesta incorrecta, Sigue intentando!");
+		pregunta_1 = false;
+		document.getElementById("respuesta_1").innerHTML='Incorrecto';
+	}
+//* segunda pregunta *//
+
+	var segunda_pregunta = prompt("¿El Bootcamp de laboratoria Es solo para mujeres?");
+	segunda_pregunta = segunda_pregunta.toLowerCase();
+	if (segunda_pregunta === "si") {
+		alert("¡Muy bien!");
+		pregunta_2 = true;
+		document.getElementById("respuesta_2").innerHTML='Correcto';
+	}else{
+		alert("!Respuesta incorrecta, Sigue intentando¡");
+		pregunta_2 = false;
+		document.getElementById("respuesta_2").innerHTML='Incorrecto';
+		}
+//* tercera pregunta *// 
+
+	var tercera_pregunta = prompt("<Laboratoria> código que transforma ¿Este es el lema de Laboratoria?");
+	tercera_pregunta = tercera_pregunta.toLowerCase();
+	if (tercera_pregunta === "si") {
+		alert("!Muy bien¡");
+		pregunta_3 = true;
+		document.getElementById("respuesta_3").innerHTML='Correcto';
+			}
+	else{
+			alert("Respuesta incorrecta, Sigue intentando");
+			pregunta_3 = false;
+			document.getElementById("respuesta_3").innerHTML='Incorrecto';
+	     }
+	alert('Veamos Resultados');
+
+		}
+//* No desea jugar *//
+else {
+	alert("¡Este reto es solo para valientes!");
+	}
+
+
